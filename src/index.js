4ea37e76170ddc923ec594bafc2c45d8355525bd
Vitor Mattos <vitor.mattos@vtex.com.br>
2016-02-24 15:58:50 -0300

    Adds parser's initial code

---
+++ src/index.js
@@ -0,0 +1,63 @@
+'use strict'
+
+var fs = require('fs')
+  , S = require('string')
+
+const _cloudConfig = Symbol('cloudConfig')
+    , _lines = Symbol('lines')
+    , _startingLine = Symbol('startingLine')
+
+class Bandit {
+  constructor(ccFile) {
+    this._refRegexp = new RegExp('\\{[ ]?"Ref":[ ]?"[a-zA-Z0-9]+"[ ]?\\}');
+    this._fnRegexp = new RegExp('\\{[ ]?"Fn::GetAtt":[ ]?\\[.*\\][ ]?\\}');
+
+    this[_cloudConfig] = fs.readFileSync(ccFile);
+    this[_lines] = S(this[_cloudConfig]).lines();
+    this[_startingLine] = 0
+  }
+
+  /**************************************
+   * Private methods                    *
+   **************************************/
+
+  _updateLines(lines, i, matcher, matches) {
+    var cursor = lines[i];
+    var lineSplit = cursor.split(matcher);
+    var updatedLine = lineSplit[0];
+    var firstMatch = matches[0];
+    var secondPart = lines[i].split(updatedLine + firstMatch)[1];
+
+    lines[i] = updatedLine;
+    lines.splice(i+1, 0, JSON.parse(firstMatch));
+    lines.splice(i+2, 0, secondPart);
+  }
+
+  _parse(lines, i) {
+    var cursor = lines[i];
+    if(typeof cursor === 'undefined') return lines;
+    if(typeof cursor === 'string') {
+      var refMatch = cursor.match(this._refRegexp);
+      var fnMatch = cursor.match(this._fnRegexp);
+
+      if(refMatch) this._updateLines(lines, i, this._refRegexp, refMatch);
+      else if(fnMatch) this._updateLines(lines, i, this._fnRegexp, fnMatch);
+      else lines[i] = lines[i] + '\n';
+    }
+
+    return this._parse(lines, i+1);
+  }
+
+  /**************************************
+   * Public methods                     *
+   **************************************/
+
+  parse() {
+    var lines = this[_lines];
+    var i = this[_startingLine];
+
+    return this._parse(lines, i);
+  }
+}
+
+module.exports = Bandit;
